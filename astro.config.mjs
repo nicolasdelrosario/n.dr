bb8ff439a69195cd576e74bd26e83d5788ef1078
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2026-08-05 23:12:59 -0500

    feat(seo): add brand identity metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nicolasdelrosario.com',
+  site: 'https://www.nicolasdelrosario.com',
   trailingSlash: 'always',
   integrations: [sitemap()],
 });
